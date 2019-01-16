c5f5c9bcc0729a5a5c9b7f6c42bfd3a9039488b8
Akiko Takano <akiko.pusu@gmail.com>
2019-01-16 15:51:41 +0900

    sample code

---
+++ japan_world.java
@@ -0,0 +1,13 @@
+public class JapanWorld {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println(test("one"));
+  }
+
+  /* PMD test: UseEqualsToCompareStrings */
+  public static boolean test(String s) {
+    if (s == "one") return true;
+    if ("two".equals(s)) return true;
+    return false; }
+}
