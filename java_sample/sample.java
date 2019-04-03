980205207a245a94b285f558084c5b527773133c
Akiko Takano <akiko.pusu@gmail.com>
2019-04-03 12:44:34 +0900

    Fix.

--- java_sample/sample.java
+++ java_sample/sample.java
@@ -1,10 +1,9 @@
-import java.util.*;
 import java.text.SimpleDateFormat;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-        sdf.parse("2014/02/29");
-        System.out.println(sdf.parse("2014/02/29").toString());
-    }
+public class Sample {
+  public static void main(String[] args) throws Exception {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+    sdf.parse("2014/02/29");
+    System.out.println(sdf.parse("2014/02/29").toString());
+  }
 }
