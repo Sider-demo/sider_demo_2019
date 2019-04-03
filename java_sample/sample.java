c4f73b69514c91d89d52b4afcc3638e31ac4ba83
Akiko Takano <akiko.pusu@gmail.com>
2019-04-03 12:50:05 +0900

    Set locale (Set locale (Pointed out by PMD)

--- java_sample/sample.java
+++ java_sample/sample.java
@@ -2,7 +2,7 @@ import java.text.SimpleDateFormat;
 
 public class Sample {
   public static void main(String[] args) throws Exception {
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd", Locale.JAPAN);
     sdf.parse("2014/02/29");
     System.out.println(sdf.parse("2014/02/29").toString());
   }
